3c0d7702e75408d68884115fb6cf3b5c524a114a
Qi LI <liqi0425@gmail.com>
2016-11-24 05:48:43 +0000

    logging

--- data_serving/data_server.ml
+++ data_serving/data_server.ml
@@ -37,8 +37,10 @@ let make_server ip port  =
         let fname = Server.resolve_file ~docroot:"." ~uri in
         Server.respond_file ~headers ~fname ()
       else if meth = `POST then
+        let () = Printf.printf "open %s\n%!" path in
         let oc = open_out path in
         Cohttp_lwt_body.to_string body >>= fun body_str ->
+        let () = Printf.printf "get string %s\n%!" body_str in
         output_string oc body_str;
         flush oc;
         close_out oc;
